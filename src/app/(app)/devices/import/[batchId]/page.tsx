b675b9ad84434ff802d58f93fc411553b21bfc71
Mathijs <mathijs@familie-brand.nl>
2026-09-07 09:50:57 +0200

    #50 Use responsive importer workspace frame

--- src/app/(app)/devices/import/[batchId]/page.tsx
+++ src/app/(app)/devices/import/[batchId]/page.tsx
@@ -1,8 +1,8 @@
-import { ImporterV2Workspace } from '@/components/devices/importer-v2-workspace'
+import { ImporterV2WorkspaceFrame } from '@/components/devices/importer-v2-workspace-frame'
 
 type ImportWorkspacePageProps = { params: Promise<{ batchId: string }> }
 
 export default async function ImportWorkspacePage({ params }: ImportWorkspacePageProps) {
   const { batchId } = await params
-  return <ImporterV2Workspace batchId={batchId} />
+  return <ImporterV2WorkspaceFrame batchId={batchId} />
 }
