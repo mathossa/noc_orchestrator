34b6704bcc4d0cf725bc867ef1256ad02ce601a6
Mathijs <mathijs@familie-brand.nl>
2026-09-23 16:51:12 +0200

    feat(#110): accept site edit deep links

--- src/app/(app)/customers/[id]/sites/page.tsx
+++ src/app/(app)/customers/[id]/sites/page.tsx
@@ -2,10 +2,16 @@ import { SiteManager } from '@/components/sites/site-manager'
 
 type PageProps = {
   params: Promise<{ id: string }>
-  searchParams: Promise<{ organizationUnit?: string }>
+  searchParams: Promise<{ organizationUnit?: string; edit?: string }>
 }
 
 export default async function CustomerSitesPage({ params, searchParams }: PageProps) {
   const [{ id }, query] = await Promise.all([params, searchParams])
-  return <SiteManager customerId={id} initialOrganizationUnit={query.organizationUnit ?? ''} />
+  return (
+    <SiteManager
+      customerId={id}
+      initialOrganizationUnit={query.organizationUnit ?? ''}
+      initialEditId={query.edit ?? ''}
+    />
+  )
 }
